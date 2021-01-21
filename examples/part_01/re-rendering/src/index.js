75e1162d8ddf51554a7868c8a4d3caf6a6378df8
Nima Azarbayjany <nimaza@gmail.com>
2021-01-21 18:21:30 +0330

    re-rendering and event handling examples from part 1c

--- examples/part_01/re-rendering/src/index.js
+++ examples/part_01/re-rendering/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Display = ({counter}) => <div>{counter}</div>;
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text={'plus'} />
+      <Button handleClick={setToZero} text={'zero'} />
+      <Button handleClick={decreaseByOne} text={'minus'} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
