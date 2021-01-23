5fe155492a722b1b5bce3bdeb498cafa6e2d5a30
Nima Azarbayjany <nimaza@gmail.com>
2021-01-23 10:07:15 +0330

    1.6: unicafe step1

--- part_01/unicafe/src/index.js
+++ part_01/unicafe/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = ({onClick, text}) => (<button onClick={onClick}>{text}</button>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const voteGood = () => setGood(good + 1);
+  const voteNeutral = () => setNeutral(neutral + 1);
+  const voteBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <Button onClick={voteGood} text='good' />
+      <Button onClick={voteNeutral} text='neutral' />
+      <Button onClick={voteBad} text='bad' />
+
+      <h1>Statistics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
