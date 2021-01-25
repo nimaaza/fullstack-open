bfd487b8c359178fe7a52b8ec9650868ce24d98c
Nima Azarbayjany <nimaza@gmail.com>
2021-01-25 18:08:53 +0330

    2.13: Data for countries, step 2

--- part_02/data_for_countries/src/App.js
+++ part_02/data_for_countries/src/App.js
@@ -13,18 +13,51 @@ const App = () => {
       .then(response => setCountries(response.data));
   }, []);
 
-  const queryChangeHandler = event => setQuery(event.target.value.trim());
+  const queryChangeHandler = event => setQuery(event.target.value);
+  const showCountryHandler = event => {
+    const country = 
+      countries.find(country => country.name === event.target.value);
+    setQuery(country.name);
+  };
 
   const countriesMatchingQuery =
     countries.filter(country =>
-      country.name.toLowerCase().includes(query.toLowerCase()));
+      country.name.toLowerCase().includes(query.trim().toLowerCase()));
 
-  return (
-    <div>
-      find countries <input value={query} onChange={queryChangeHandler} />
-      <DisplayCountry query={query} countries={countriesMatchingQuery} />
-    </div>
-  );
+  if (query === '') {
+    return <div>find countries <input value={query} onChange={queryChangeHandler} /></div>;
+  } else if (countriesMatchingQuery.length > 10) {
+    return (
+      <div>
+        find countries <input value={query} onChange={queryChangeHandler} />
+        <p>Too many matches, provide a more specific query.</p>
+      </div>
+    );
+  } else if (countriesMatchingQuery.length > 1) {
+    return (
+      <div>
+        find countries <input value={query} onChange={queryChangeHandler} />
+        {countriesMatchingQuery.map(country =>
+          <div key={country.name}>
+            {country.name} <button onClick={showCountryHandler} value={country.name}>show</button>
+          </div>)}
+      </div>
+    );
+  } else if (countriesMatchingQuery.length === 1) {
+    return (
+      <div>
+        find countries <input value={query} onChange={queryChangeHandler} />
+        <DisplayCountry country={countriesMatchingQuery[0]} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        find countries <input value={query} onChange={queryChangeHandler} />
+        <p>no matches found.</p>
+      </div>
+    );
+  }
 };
 
 export default App;
--- part_02/data_for_countries/src/components/DisplayCountry.js
+++ part_02/data_for_countries/src/components/DisplayCountry.js
@@ -1,34 +1,22 @@
 import React from 'react';
 
-const DisplayCountry = ({ query, countries }) => {
-  if (query.length === 0) return <p></p>;
-
-  if (countries.length > 10) {
-    return <p>Too many matches, provide a more specific query.</p>;
-  } else if (countries.length > 1) {
-    return countries.map(country =>
-      <p key={country.name}>{country.name}</p>);
-  } else if (countries.length === 1) {
-    const country = countries[0];
-    return (
-      <div>
-        <h1>{country.name}</h1>
-        <p>capital {country.capital}</p>
-        <p>population {country.population}</p>
-        <h2>Languages</h2>
-        <ul>
-          {country.languages.map(language =>
-            <li key={`${country.name} ${language.name}`}>
-              {language.name}
-            </li>
-          )}
-        </ul>
-        <img src={country.flag} height='100px' alt='' />
-      </div>
-    );
-  } else {
-    return <p>no matches found.</p>;
-  }
+const DisplayCountry = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map(language =>
+          <li key={`${country.name} ${language.name}`}>
+            {language.name}
+          </li>
+        )}
+      </ul>
+      <img src={country.flag} height='100px' alt='' />
+    </div>
+  );
 };
 
 export default DisplayCountry;
