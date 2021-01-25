50148481b19d2742f18af2ea06339657d869034f
Nima Azarbayjany <nimaza@gmail.com>
2021-01-25 12:57:31 +0330

    2.12 Data for countries, step 1

--- part_02/data_for_countries/src/App.js
+++ part_02/data_for_countries/src/App.js
@@ -1,7 +1,30 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-const App = () => {
+import DisplayCountry from './components/DisplayCountry';
 
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => setCountries(response.data));
+  }, []);
+
+  const queryChangeHandler = event => setQuery(event.target.value.trim());
+
+  const countriesMatchingQuery =
+    countries.filter(country =>
+      country.name.toLowerCase().includes(query.toLowerCase()));
+
+  return (
+    <div>
+      find countries <input value={query} onChange={queryChangeHandler} />
+      <DisplayCountry query={query} countries={countriesMatchingQuery} />
+    </div>
+  );
 };
 
 export default App;
