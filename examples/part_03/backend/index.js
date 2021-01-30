0bdab6ab65f221b12098b10dcb8cb5dccf091d33
Nima Azarbayjany <nimaza@gmail.com>
2021-01-30 21:04:56 +0330

    Add middleware functions to server

--- examples/part_03/backend/index.js
+++ examples/part_03/backend/index.js
@@ -1,7 +1,17 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:', request.path);
+  console.log('Body:', request.body);
+  console.log('---');
+  next();
+};
+
 app.use(express.json());
+app.use(requestLogger);
 
 let notes = [
   {
@@ -83,3 +93,9 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
