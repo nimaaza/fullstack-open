e9eaaad546260baac3add4563b11a09bf83fb6c7
Nima Azarbayjany <nimaza@gmail.com>
2021-02-01 07:58:16 +0330

    3.4: Phonebook backend step 4

--- part_03/phonebook/index.js
+++ part_03/phonebook/index.js
@@ -2,7 +2,7 @@ const { request, response } = require('express');
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -40,6 +40,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  
+  response.status(204).end();
 });
 
 const PORT = 3001;
