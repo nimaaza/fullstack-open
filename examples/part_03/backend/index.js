63e45af986379fad9d2adeaa347efb70cee8c6f6
Nima Azarbayjany <nimaza@gmail.com>
2021-02-04 10:18:20 +0330

    Use MongoDB in the backend for the notes app

--- examples/part_03/backend/index.js
+++ examples/part_03/backend/index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const Note = require('./models/note');
+
 const app = express();
 
 const requestLogger = (request, response, next) => {
@@ -15,44 +19,13 @@ app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello, world!</h1>');
-});
-
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => response.json(notes));
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id)
+    .then(note => response.json(note));
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -63,35 +36,24 @@ app.delete('/api/notes/:id', (request, response) => {
 });
 
 app.post('/api/notes', (request, response) => {
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(note => note.id))
-      : 0;
-
-    return maxId + 1;
-  };
-
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes.concat(note);
-
-  response.json(note);
+  note.save().then(savedNote => response.json(savedNote));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
