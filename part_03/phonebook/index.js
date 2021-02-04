1f3f92c436935c9a6dcad6d580e8bd63ebe700ea
Nima Azarbayjany <nimaza@gmail.com>
2021-02-04 19:31:47 +0330

    3.15: Phonebook database, step 3

--- part_03/phonebook/index.js
+++ part_03/phonebook/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const Person = require('./models/person');
 const cors = require('cors');
 const morgan = require('morgan');
+const { request, response } = require('express');
 
 const app = express();
 
@@ -26,6 +27,16 @@ app.use(morgan((tokens, request, response) => {
   return logMessage.join(' ');
 }));
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' });
+  }
+
+  next(error);
+};
+
 const isNameAlreadyTaken = (person) =>
   persons.find(p => p.name === person.name);
 
@@ -39,21 +50,22 @@ app.get('/info', (request, response) => {
   response.send(html);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => response.status(204).end())
-    .catch(error => console.log(error.message));
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -73,3 +85,5 @@ app.post('/api/persons', (request, response) => {
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(errorHandler);
