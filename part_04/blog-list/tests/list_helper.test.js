162726a8a564c86aa040ab431686186beb79ef75
Nima Azarbayjany <nimaza@gmail.com>
2021-02-07 17:28:25 +0330

    4.4: helper functions and unit tests, step 2

--- part_04/blog-list/tests/list_helper.test.js
+++ part_04/blog-list/tests/list_helper.test.js
@@ -5,3 +5,57 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const blogs = [
+    {
+      id: '601fa9277f31e518453f2ba2',
+      title: 'Helpful tips',
+      author: 'Mr. Nice',
+      url: 'http://www.mrnice.com/blog/12',
+      likes: 112,
+      v: 0,
+    },
+    {
+      id: '601fa9c97f31e518453f2ba3',
+      title: 'Seven things to learn',
+      author: 'Mr. Nice',
+      url: 'http://www.mrnice.com/blog/12',
+      likes: 144,
+      v: 0,
+    },
+    {
+      id: '601fb5e5c41a4d1f2d93b502',
+      title: 'Try to have fun',
+      author: 'Mr. Nice',
+      url: 'http://www.mrnice.com/blog/14',
+      likes: 173,
+      v: 0,
+    },
+  ];
+
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0);
+  });
+
+  test('when list has only one blog, equals the likes of that', () => {
+    expect(listHelper.totalLikes(listWithOneBlog))
+      .toBe(listWithOneBlog[0].likes);
+  });
+
+  test('of a bigger list is correctly calculated', () => {
+    expect(listHelper.totalLikes(blogs))
+      .toBe(429);
+  });
+});
