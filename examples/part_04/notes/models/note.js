a0b0e420de0c3df8b91dde1bb1cc5e7da9002fdf
Nima Azarbayjany <nimaza@gmail.com>
2021-02-07 09:27:24 +0330

    Copy notes app from previous parts to continue work

---
+++ examples/part_04/notes/models/note.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URL;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(result => console.log('connected to MongoDB'))
+  .catch(error => console.log('error connecting to MongoDB:', error));
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
+});
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedDocument) => {
+    returnedDocument.id = returnedDocument._id.toString();
+    delete returnedDocument._id;
+    delete returnedDocument.__v;
+  }
+});
+
+module.exports = mongoose.model('Note', noteSchema);
