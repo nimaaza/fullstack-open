5d69232b5975bfbc07adc1feccc27e9808e4ce35
Nima Azarbayjany <nimaza@gmail.com>
2021-02-07 19:24:17 +0330

    4.6: helper functions and unit tests, step 4

--- part_04/blog-list/tests/list_helper.test.js
+++ part_04/blog-list/tests/list_helper.test.js
@@ -36,6 +36,46 @@ const blogs = [
     likes: 173,
     v: 0,
   },
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0,
+  },
 ];
 
 test('dummy returns one', () => {
@@ -56,7 +96,7 @@ describe('total likes', () => {
 
   test('of a bigger list is correctly calculated', () => {
     expect(listHelper.totalLikes(blogs))
-      .toBe(429);
+      .toBe(465);
   });
 });
 
@@ -83,5 +123,31 @@ describe('favorite blog', () => {
   test('correctly find the most favorite blog in list of blogs', () => {
     expect(listHelper.favoriteBlog(blogs))
       .toEqual(favorite);
+  });
+});
+
+describe('author with most blogs', () => {
+  test('handle empty list of blogs by returning undefined', () => {
+    expect(listHelper.mostBlogs([]))
+      .toEqual({
+        author: undefined,
+        blogs: undefined,
+      });
+  });
+
+  test('return the author in the list of just one', () => {
+    expect(listHelper.mostBlogs(listWithOneBlog))
+      .toEqual({
+        author: 'Edsger W. Dijkstra',
+        blogs: 1,
+      });
+  });
+
+  test('return the correct author with most blogs in a larger list', () => {
+    expect(listHelper.mostBlogs(blogs))
+      .toEqual({
+        author: 'Mr. Nice',
+        blogs: 3,
+      });
   });
 });
--- part_04/blog-list/utils/list_helper.js
+++ part_04/blog-list/utils/list_helper.js
@@ -14,8 +14,30 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(reducer, blogs[0]);
 };
 
+const mostBlogs = (blogs) => {
+  const authors = {};
+
+  blogs.forEach(blog => {
+    if (authors[blog.author]) authors[blog.author] += 1;
+    else authors[blog.author] = 1;
+  });
+
+  const authorsList = Object.keys(authors);
+  const authorWithMaxBlogs = authorsList.reduce((maxBlogs, nextAuthor) => {
+    const moreBlogs = authors[nextAuthor] > authors[maxBlogs]
+      ? nextAuthor : maxBlogs;
+    return moreBlogs;
+  }, authorsList[0]);
+
+  return {
+    author: authorWithMaxBlogs,
+    blogs: authors[authorWithMaxBlogs],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
