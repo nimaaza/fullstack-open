8903b58c0d0180bc36ea1b359f404913cc51af8c
Nima Azarbayjany <nimaza@gmail.com>
2021-02-10 17:18:40 +0330

    4.11: Blog list tests, step 4

--- part_04/blog-list/controllers/blogs.js
+++ part_04/blog-list/controllers/blogs.js
@@ -7,10 +7,14 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
+  if (!request.body.likes) {
+    request.body.likes = 0;
+  }
+
   const blog = new Blog(request.body);
 
   const savedBlog = await blog.save();
-  response.status(201).json(savedBlog);
+  response.json(savedBlog);
 });
 
 module.exports = blogsRouter;
--- part_04/blog-list/tests/blog_api.test.js
+++ part_04/blog-list/tests/blog_api.test.js
@@ -35,7 +35,7 @@ test('the unique identifier id exists for blog items', async () => {
 
 test('a valid blog can be added', async () => {
   const blog = new Blog({
-    title: 'New interesting blog post',
+    title: 'New interesting blog',
     author: 'Mr. Authormann',
     url: 'http://www.authors.org',
     likes: 207,
@@ -43,10 +43,24 @@ test('a valid blog can be added', async () => {
 
   await blog.save();
   const blogs = (await api.get('/api/blogs')).body;
-  const titles = blogs.map(blog => blog.title);
+  const titles = blogs.map(b => b.title);
 
   expect(blogs).toHaveLength(helper.blogs.length + 1);
-  expect(titles).toContain('New interesting blog post');
+  expect(titles).toContain('New interesting blog');
+});
+
+test('a blog without a value of likes is saved with 0 likes', async () => {
+  const blog = {
+    title: 'New blog with no likes saved for testing',
+    author: 'Mr. Authormann',
+    url: 'http://www.authors.org',
+  };
+
+  await api.post('/api/blogs').send(blog);
+  const blogs = (await api.get('/api/blogs')).body;
+  const savedBlog = blogs
+    .find(b => b.title === 'New blog with no likes saved for testing');
+  expect(savedBlog.likes).toBe(0);
 });
 
 afterAll(() => {
