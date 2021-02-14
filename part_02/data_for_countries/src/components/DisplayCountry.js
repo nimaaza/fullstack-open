7c6f37239f0bcbf45e8b2fa46091f4b93543987f
Nima Azarbayjany <nimaza@gmail.com>
2021-02-14 17:00:34 +0330

    2.14: Data for countries, step 3

--- part_02/data_for_countries/src/components/DisplayCountry.js
+++ part_02/data_for_countries/src/components/DisplayCountry.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const DisplayCountry = ({ country }) => {
+  const [temperature, setTemperature] = useState(0);
+  const [weatherIcon, setWeatherIcon] = useState('');
+  const [weatherDescription, setWeatherDescription] = useState('');
+
+  const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+
+  useEffect(() => {
+    axios
+      .get(weatherUrl)
+      .then(response => {
+        const weatherData = response.data;
+        setTemperature(Math.round(weatherData.main.temp));
+        setWeatherIcon(`https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/${weatherData.weather[0].icon}.png`);
+        setWeatherDescription(weatherData.weather[0].description);
+      });
+  });
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -14,7 +32,11 @@ const DisplayCountry = ({ country }) => {
           </li>
         )}
       </ul>
-      <img src={country.flag} height='100px' alt='' />
+      <img src={country.flag} height='100px' alt='' border='1px solid'/>
+      <h2>Weather in {country.capital}</h2>
+      <p><strong>temperature:</strong> {temperature}</p>
+      <img src={weatherIcon} alt='weather icon' />
+      <p>{weatherDescription}</p>
     </div>
   );
 };
