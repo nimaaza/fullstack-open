0df5dfec1809b841301690c8f1037e8fb3e39177
Nima Azarbayjany <nimaza@gmail.com>
2021-02-14 08:55:09 +0330

    4.19: bloglist expansion, step 7

--- part_04/blog-list/controllers/blogs.js
+++ part_04/blog-list/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog.js');
 const User = require('../models/user');
+
+const getToken = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {
@@ -12,12 +22,20 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
+  const token = getToken(request);
+  const decrypredToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decrypredToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid',
+    });
+  }
+
   if (!request.body.likes) {
     request.body.likes = 0;
   }
 
-  const user = await User.findById(request.body.user);
-
+  const user = await User.findById(decrypredToken.id);
   const blog = new Blog({ ...request.body, user: user._id });
   const savedBlog = await blog.save();
 
