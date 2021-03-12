bf7bafa2dfef386918fc8b703b71098b02d142c1
Nima Azarbayjany <nimaza@gmail.com>
2021-03-12 08:54:41 +0330

    5.7 Blog list frontend, step 7

--- part_05/bloglist-frontend/src/components/Blog.js
+++ part_05/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+
+  if (showDetails) {
+    return (
+      <div className="blog-item">
+        <p>{blog.title} <button onClick={() => setShowDetails(false)}>hide</button></p>
+        {blog.url}
+        <p>likes {blog.likes} <button>like</button></p>
+        {blog.author}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>{blog.title} <button onClick={() => setShowDetails(true)}>view</button></p>
+      </div>
+    );
+  }
+};
 
 export default Blog;
