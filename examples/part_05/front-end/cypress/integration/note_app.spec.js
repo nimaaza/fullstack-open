bd79f74e7f456cc31b01756fe50ae2a46692ed08
Nima Azarbayjany <nimaza@gmail.com>
2021-03-19 15:46:43 +0330

    Setup Cypress testing library and write basic tests

---
+++ examples/part_05/front-end/cypress/integration/note_app.spec.js
@@ -0,0 +1,12 @@
+describe('Note app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000');
+    cy.contains('Notes');
+    cy.contains('Note app, Depart. of Computer Science, University of Helsinki 2020');
+  });
+
+  // it('front page contains random text', function(){
+  //   cy.visit('http://localhost:3000');
+  //   cy.contains('wtf is this app?');
+  // });
+});
