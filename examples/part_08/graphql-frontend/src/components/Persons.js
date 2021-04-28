876f3486cf10fb0d5f5b0a6541d5c444cb7e1a51
Nima Azarbayjany <nimaza@gmail.com>
2021-04-28 12:33:48 +0430

    Use named queries, variables, and  useLazyQuery hook function

--- examples/part_08/graphql-frontend/src/components/Persons.js
+++ examples/part_08/graphql-frontend/src/components/Persons.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { gql, useLazyQuery } from '@apollo/client';
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
 
 const Persons = ({ persons }) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON);
+  const [person, setPerson] = useState(null);
+
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name }});
+  };
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson);
+    }
+  }, [result]);
+
+  if (person) {
+    return (
+      <div>
+        <h2>{person.name}</h2>
+        <div>{person.address.street} {person.address.city}</div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>Persons</h2>
@@ -8,6 +47,9 @@ const Persons = ({ persons }) => {
       {persons.map(p =>
         <div key={p.name}>
           {p.name} {p.phone}
+          <button onClick={() => showPerson(p.name)}>
+            show address
+          </button>
         </div>
       )}
     </div>
