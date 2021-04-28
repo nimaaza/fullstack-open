a01f7bed109ab6a958704053ddfa237ff0e72f45
Nima Azarbayjany <nimaza@gmail.com>
2021-04-28 11:32:57 +0430

    Basic Apollo client in React

--- examples/part_08/graphql-frontend/src/index.js
+++ examples/part_08/graphql-frontend/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+});
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+client.query({ query })
+  .then(response => {
+    console.log(response.data);
+  });
+
+ReactDOM.render(<App />, document.getElementById('root'));
