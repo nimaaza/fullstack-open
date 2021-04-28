aa37af339ef519380343612cb0a7af5a68856643
Nima Azarbayjany <nimaza@gmail.com>
2021-04-28 12:10:00 +0430

    Use useQuery hook functions to query the GraphQL server

---
+++ examples/part_08/graphql-frontend/src/components/Persons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Persons</h2>
+
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Persons;
