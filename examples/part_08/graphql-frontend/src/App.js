cf7ec77e0d2059bd8d59dc9b76e290156171970e
Nima Azarbayjany <nimaza@gmail.com>
2021-04-29 12:27:09 +0430

    Enable changing of numbers for existing persons

--- examples/part_08/graphql-frontend/src/App.js
+++ examples/part_08/graphql-frontend/src/App.js
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import PhoneForm from './components/PhoneForm';
 import Notify from './components/Notify';
 
 import { ALL_PERSONS } from './queries';
@@ -30,6 +31,7 @@ const App = () => {
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   );
 }
