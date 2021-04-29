c6668c98e7127954df316a4cf5c9f8e38ba61b01
Nima Azarbayjany <nimaza@gmail.com>
2021-04-29 11:58:08 +0430

    Handle mutation errors

--- examples/part_08/graphql-frontend/src/App.js
+++ examples/part_08/graphql-frontend/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
+
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import Notify from './components/Notify';
 
 import { ALL_PERSONS } from './queries';
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS);
 
   // This code refreshes the cache every 2 seconds but causes
   // lots of pointless web traffic.
   // const result = useQuery(ALL_PERSONS, { pollInterval: 2000 });
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => setErrorMessage(null), 10000);
+  };
 
   if (result.loading) {
     return <div>loading...</div>
@@ -18,9 +27,9 @@ const App = () => {
 
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 }
