ca2f387f0372bc25ae277d81419498526da7c828
Nima Azarbayjany <nimaza@gmail.com>
2021-05-09 21:04:56 +0430

    Practice state management with Redux

--- examples/part_06/flux-redux/src/App.js
+++ examples/part_06/flux-redux/src/App.js
@@ -1,5 +1,23 @@
-const App = () => {
-  return null;
-}
+const App = ({ store }) => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+
+      <button onClick={e => store.dispatch({ type: 'INCREMENT' })}>
+        plus
+      </button>
+
+      <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>
+        minus
+      </button>
+
+      <button onClick={e => store.dispatch({ type: 'ZERO' })}>
+        zero
+      </button>
+    </div>
+  );
+};
 
 export default App;
--- examples/part_06/flux-redux/src/index.js
+++ examples/part_06/flux-redux/src/index.js
@@ -2,4 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    case 'ZERO': return 0;
+    default: return state
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+/*
+Demonstrate dispatching actions & subscribing to state changes:
+
+console.log(store.getState());
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+
+console.log(store.getState());
+
+store.dispatch({ type: 'ZERO' });
+store.dispatch({ type: 'DECREMENT' });
+
+console.log(store.getState());
+
+store.dispatch({ type: 'ZERO' });
+*/
+
+const renderApp = () => {
+  ReactDOM.render(<App store={store} />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
