160d855c6ba2967ef4c472289198b715a1c393f8
Nima Azarbayjany <nimaza@gmail.com>
2021-05-10 10:47:37 +0430

    Uncontrolled forms and action creators

--- examples/part_06/redux-notes/src/App.js
+++ examples/part_06/redux-notes/src/App.js
@@ -1,9 +1,28 @@
+import { createNote, toggleImportanceOf } from './reducers/noteReducer';
+
 const App = ({ store }) => {
+  const addNote = (event) => {
+    event.preventDefault();
+
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    store.dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id));
+  };
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
+
       <ul>
         {store.getState().map(note =>
-          <li key={note.id}>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
