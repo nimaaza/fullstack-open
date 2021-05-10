a1e4f56bdfd8778fbac77f83d0e51d2ace795773
Nima Azarbayjany <nimaza@gmail.com>
2021-05-10 11:36:39 +0430

    Refactoring app into more components

--- examples/part_06/redux-notes/src/App.js
+++ examples/part_06/redux-notes/src/App.js
@@ -1,40 +1,15 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 
-import { createNote, toggleImportanceOf } from './reducers/noteReducer';
+import NoteForm from './components/NoteForm';
+import Notes from './components/Notes';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const notes = useSelector(state => state);
-
-  const addNote = (event) => {
-    event.preventDefault();
-
-    const content = event.target.note.value;
-    event.target.note.value = '';
-    dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id));
-  };
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name='note' />
-        <button type='submit'>add</button>
-      </form>
-
-      <ul>
-        {notes.map(note =>
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NoteForm />
+      <Notes />
     </div>
   );
-}
+};
 
 export default App;
