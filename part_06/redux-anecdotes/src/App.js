17f799aa3f4c3d8c6d542c0686a15a89da416066
Nima Azarbayjany <nimaza@gmail.com>
2021-05-11 16:40:45 +0430

    6.13 Anecdotes and the backend, step 1

--- part_06/redux-anecdotes/src/App.js
+++ part_06/redux-anecdotes/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
+import anecdoteService from './services/anecdotes';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(response => dispatch(initializeAnecdotes(response)));
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
--- part_06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
@@ -17,10 +8,12 @@ const asObject = (anecdote) => {
   };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
+    case 'INIT_ANECDOTES': {
+      return action.data;
+    }
+
     case 'VOTE': {
       const id = action.data.id;
       const votedAnecdote = state.find(a => a.id === id);
@@ -41,6 +34,13 @@ const reducer = (state = initialState, action) => {
   }
 }
 
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
+  };
+};
+
 export const voteAnecdote = (id) => {
   return {
     type: 'VOTE',
@@ -51,9 +51,7 @@ export const voteAnecdote = (id) => {
 export const createAnecdote = (anecdote) => {
   return {
     type: 'CREATE_NEW',
-    data: {
-      anecdote,
-    },
+    data: { anecdote },
   };
 };
 
