b7f1d6a0f6caba4624dfe4c167105e23db7203fc
Nima Azarbayjany <nimaza@gmail.com>
2021-05-11 16:01:41 +0430

    Communicate with server in a Redux application

--- examples/part_06/redux-notes/src/App.js
+++ examples/part_06/redux-notes/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import noteService from './services/notes';
+import {initializeNotes} from './reducers/noteReducer';
 import NoteForm from './components/NoteForm';
 import Notes from './components/Notes';
 import VisibilityFilter from './components/VisibilityFiler';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(notes => dispatch(initializeNotes(notes)));
+  }, [dispatch]);
+
   return (
     <div>
       <NoteForm />
