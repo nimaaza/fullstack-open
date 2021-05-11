b570af8d92337ce8d824d91d437d78f18008efaf
Nima Azarbayjany <nimaza@gmail.com>
2021-05-11 23:53:21 +0430

    6.15 Anecdotes and the backend, step 3

--- part_06/redux-anecdotes/src/App.js
+++ part_06/redux-anecdotes/src/App.js
@@ -5,16 +5,13 @@ import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
-import anecdoteService from './services/anecdotes';
 import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(response => dispatch(initializeAnecdotes(response)));
+    dispatch(initializeAnecdotes());
   }, [dispatch]);
 
   return (
--- part_06/redux-anecdotes/src/store.js
+++ part_06/redux-anecdotes/src/store.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import anecdoteReducer from './reducers/anecdoteReducer';
@@ -11,6 +12,9 @@ const reducer = combineReducers({
   filter: filterReducer,
 });
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
