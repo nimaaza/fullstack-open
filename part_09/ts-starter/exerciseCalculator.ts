13c84c969fdb00d8d15f9f11171223e5359252d5
Nima Azarbayjany <nimaza@gmail.com>
2021-11-10 11:12:18 +0100

    9.7 WebExercises

--- part_09/ts-starter/exerciseCalculator.ts
+++ part_09/ts-starter/exerciseCalculator.ts
@@ -78,3 +78,5 @@ try {
 }
 
 // console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+
+export { calculateExercises };
--- part_09/ts-starter/index.ts
+++ part_09/ts-starter/index.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
@@ -10,7 +16,7 @@ app.get('/hello', (_req, res) => {
 app.get('/bmi', (req, res) => {
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
-  
+
   if (isNaN(height) || isNaN(weight)) {
     res.status(500).json({
       error: 'malformed parameters'
@@ -22,6 +28,21 @@ app.get('/bmi', (req, res) => {
       bmi: calculateBmi(height, weight),
     });
   }
+});
+
+app.post('/exercises', (req, res) => {
+  if (!req.body.daily_exercises || !req.body.target) {
+    res.status(400).json({ error: 'missing parameters' }).end();
+  }
+
+  const dailyHours: Array<number> = req.body.daily_exercises.map(Number);
+  const target = Number(req.body.target);
+
+  if (isNaN(target) || dailyHours.some(isNaN)) {
+    res.status(400).json({ error: 'malformed parameters' }).end();
+  }
+
+  res.json(calculateExercises(dailyHours, target)).end();
 });
 
 const PORT = 3000;
