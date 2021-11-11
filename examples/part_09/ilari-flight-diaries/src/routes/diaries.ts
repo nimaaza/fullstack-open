19d873b5b8de23b92e1663444c672d54e63c4015
Nima Azarbayjany <nimaza@gmail.com>
2021-11-11 17:27:23 +0100

    Adding a new diary via HTTP POST

--- examples/part_09/ilari-flight-diaries/src/routes/diaries.ts
+++ examples/part_09/ilari-flight-diaries/src/routes/diaries.ts
@@ -1,14 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import express from 'express';
 import diaryService from '../services/diaryServices';
 
 const router = express.Router();
 
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 router.get('/', (_req, res) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary');
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body;
+  const newDiaryEntry = diaryService.addEntry({
+    date,
+    weather,
+    visibility,
+    comment,
+  });
+
+  res.json(newDiaryEntry);
 });
 
 export default router;
--- examples/part_09/ilari-flight-diaries/src/services/diaryServices.ts
+++ examples/part_09/ilari-flight-diaries/src/services/diaryServices.ts
@@ -1,10 +1,20 @@
+import diaryEntries from '../../data/diaries';
 import diaryData from '../../data/diaries';
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import {
+  DiaryEntry,
+  NewDiaryEntry,
+  NonSensitiveDiaryEntry,
+} from '../types';
 
 const diaries: Array<DiaryEntry> = diaryData;
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
@@ -13,12 +23,19 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   });
 };
 
-const addEntry = () => {
-  return null;
+const addEntry = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaryEntries.map(e => e.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
+  findById,
   getNonSensitiveEntries,
   addEntry,
 };
