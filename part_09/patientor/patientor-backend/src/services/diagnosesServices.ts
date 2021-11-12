765a696da10513560da33bb2a8a7d3af507f801f
Nima Azarbayjany <nimaza@gmail.com>
2021-11-12 09:17:12 +0100

    9.10: Patientor backend, step3

---
+++ part_09/patientor/patientor-backend/src/services/diagnosesServices.ts
@@ -0,0 +1,10 @@
+import diagnosesData from '../../data/diagnoses.json';
+import { Diagnose } from '../types';
+
+const getDiagnoses = (): Diagnose[] => {
+  return diagnosesData;
+};
+
+export default {
+  getDiagnoses,
+};
