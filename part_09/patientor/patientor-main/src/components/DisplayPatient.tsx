c6a5482df61dc5324a9eeee6aaa6d345a180445e
Nima Azarbayjany <nimaza@gmail.com>
2021-11-15 18:49:16 +0100

    9.17: patientor, step 2 & step 3

---
+++ part_09/patientor/patientor-main/src/components/DisplayPatient.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router";
+
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { addPatient, useStateValue } from "../state";
+
+const DisplayPatient = () => {
+  const [state, dispatch] = useStateValue();
+
+  const getPatient = async (id: string) => {
+    const { data: patient } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+    return patient;
+  };
+
+  const { id } = useParams<{ id: string }>();
+
+  if (!state.patients[id] || !state.patients[id].ssn) {
+    void getPatient(id).then(patient => {
+      dispatch(addPatient(patient));
+    });
+  }
+
+  return (
+    <div>
+      <h1>{state.patients[id].name}</h1>
+      <p>gender: {state.patients[id].gender}</p>
+      <p>ssn: {state.patients[id].ssn}</p>
+      <p>occuption: {state.patients[id].occupation}</p>
+    </div>
+  );
+};
+
+export default DisplayPatient;
--- part_09/patientor/patientor-main/src/state/reducer.ts
+++ part_09/patientor/patientor-main/src/state/reducer.ts
@@ -11,6 +11,20 @@ export type Action =
       payload: Patient;
     };
 
+export const setPatientList = (patientList: Patient[]): Action => {
+  return {
+    type: "SET_PATIENT_LIST",
+    payload: patientList,
+  };
+};
+
+export const addPatient = (patient: Patient): Action => {
+  return {
+    type: 'ADD_PATIENT',
+    payload: patient,
+  };
+};
+
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "SET_PATIENT_LIST":
